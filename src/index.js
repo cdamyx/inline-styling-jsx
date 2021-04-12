ea1a653168d1525428c279589c2af79d2057a33f
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2021-04-12 18:51:19 -0500

    goofin around

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const customStyle = {
-  color: "red",
-  fontSize: "20px",
-  border: "1px solid black"
+  color: "yellow",
+  fontSize: "30px",
+  backgroundColor: "blue",
+  border: "5px solid black",
+  textAlign: "center"
 };
 
-customStyle.color = "blue";
+customStyle.margin = "50px";
 
 ReactDOM.render(
-  <h1 style={customStyle}>Hello World!</h1>,
+  <h1 style={customStyle}>hello world</h1>,
   document.getElementById("root")
 );
